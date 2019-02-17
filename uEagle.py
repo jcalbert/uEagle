f559b7ecbfac1ce3724c377f644715400e1246ec
Joseph Albert <4261275+jcalbert@users.noreply.github.com>
2019-02-16 23:08:06 -0500

    Added some notes.  Added functions to convert responses from hex strings to numbers, apply appropriate operations, and discard superfluous info.

--- uEagle.py
+++ uEagle.py
@@ -7,12 +7,20 @@ __version__ = ''
 from ubinascii import b2a_base64
 import urequests
 import ujson
+import utime
 
 ADDR_TEMPLATE = r'http://eagle-{0}.local/cgi-bin/post_manager'
 
 CMD_TOP_TEMPLATE = r'''<Command>\n
                    <Name>{0!s}</Name>\n
                    <Format>JSON</Format>'''
+
+from sys import platform
+if platform in ('linux', 'unix'):
+    EPOCH_DELTA = 946684800
+else:
+    EPOCH_DELTA = 0
+del(platform)
 
 class Eagle(object):
     def __init__(self, cloud_id, install_code):
@@ -84,13 +92,56 @@ def encode_basic_auth(username, password):
     formated = b2a_base64(formated)[:-1].decode("ascii")
     return 'Basic {}'.format(formated)
 
+def convert_response(d):
+    '''
+    Given a response dict from the EAGLE, interpret common data
+    types / apply conversions.
+    '''
+    #Handle nested dictionaries
+    for k,v in d.items():
+        if isinstance(v,dict):
+            convert_response(v)
+    #Summation and demand conversion
+    if 'Multiplier' in d:
+        convert_demand(d)
+
+    if 'Price' in d:
+        convert_price(d)
+
+    if 'TimeStamp' in d:
+        d['TimeStamp'] = utime.localtime(int(d['TimeStamp']) + EPOCH_DELTA)
+
+def convert_demand(d):
+    factor = max(int(d['Multiplier']), 1) / max(int(d['Divisor']), 1)
+
+    if 'Demand' in d:
+        d['Demand'] = int(d['Demand']) * factor
+    else:
+        d['SummationDelivered'] = int(d['SummationDelivered']) * factor
+        d['SummationReceived'] = int(d['SummationReceived']) * factor
+
+    del(d['Multiplier'])
+    del(d['Divisor'])
+    del(d['DigitsRight'])
+    del(d['DigitsLeft'])
+    del(d['SuppressLeadingZero'])
+
+def convert_price(d):
+    d['Price'] = int(d['Price']) / 10**int(d['TrailingDigits'])
+    d['Currency'] = int(d['Currency'])
+
+    del(d['TrailingDigits'])
 
 #notes
 #More recent API (1.1) supports command list_network
-#list_network does not return JSON
-#gets info on all network interfaces
-#get_demand_peaks
+##list_network does not return JSON
+##gets info on all network interfaces
+#
+#When running locally, API Commands to not actually require MAC address
 
+#Since this is python3 compatible, we assumme int / int = float
+
+#the EAGLE's Epoch is 01-01-2000, rather than unix's 1970.
 #Each command needs:
 # POST with
 ## headers: auth
