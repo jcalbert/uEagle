02bac5df4ee2df9ff08abe8481dfcd30032f7cab
Joseph Albert <4261275+jcalbert@users.noreply.github.com>
2020-11-03 00:27:21 -0500

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='uEagle',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
 
     install_requires=REQUIRED,
